50dad9ccdae38a9b5c1060095f17fb5e8f8ece98
NouveauReece <nouveaureece@protonmail.com>
2025-01-14 13:45:32 -0500

    Added tedx-wix-share-button

---
+++ components.js
@@ -0,0 +1,16 @@
+class WixWebShareButton extends HTMLElement {
+    static observedAttributes = ["label", "share-data"];
+    constructor() {super();}
+    attributeChangedCallback(name, oldValue, newValue) { render() }
+    connectedCallback() { this.render(); }
+    attributeChangedCallback() { this.render(); }
+    render() {
+        const c = `<div data-testid="linkElement" class="VU4Mnk wixui-button wixui-tedx-button tedx-button wixui-forty-vw forty-vw wixui-no-arrow no-arrow wixui-webshare-button webshare-button PlZyDq" aria-disabled="false"><span class="w4Vxx6 wixui-button__label">${this.getAttribute("label")}</span></div>`
+        this.innerHTML = c;
+        this.addEventListener('click', async () => {
+            try { await navigator.share(this.getAttribute("share-data"));}
+            catch (err) {console.error(`Web Share API Error: ${err}`);}
+        })
+    }
+}
+customElements.define("tedx-wix-share-button", WixWebShareButton);
