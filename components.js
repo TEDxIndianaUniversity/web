01c3cdfab24987fbabfc28e4e18f86d487b975c4
NouveauReece <nouveaureece@protonmail.com>
2025-01-14 14:34:46 -0500

    Changed Wix Classes

--- components.js
+++ components.js
@@ -13,8 +13,10 @@ class WixWebShareButton extends HTMLElement {
     }
     attributeChangedCallback() { this.render(); }
     render() {
-        const c = `<div data-testid="linkElement" class="VU4Mnk wixui-button wixui-tedx-button tedx-button wixui-forty-vw forty-vw wixui-no-arrow no-arrow wixui-webshare-button webshare-button PlZyDq" aria-disabled="false"><span class="w4Vxx6 wixui-button__label">${this.getAttribute("label")}</span></div>`
+        const c = `<div class="comp-m5vn07vb3 UHSxqn" id="comp-m5vn07vb3" role="button" tabindex="0" aria-disabled="false"><div data-testid="linkElement" class="VU4Mnk wixui-button wixui-tedx-button tedx-button wixui-forty-vw forty-vw wixui-no-arrow no-arrow wixui-webshare-button webshare-button PlZyDq" aria-disabled="false"><span class="w4Vxx6 wixui-button__label">${this.getAttribute("label")}</span></div></div>`
         this.innerHTML = c;
+
+        
     }
 }
 customElements.define("tedx-wix-share-button", WixWebShareButton);
