12efbb4452c058f5f57a912d3e06c543029190d0
NouveauReece <nouveaureece@protonmail.com>
2025-01-14 14:28:05 -0500

    Fixed Web Share

--- components.js
+++ components.js
@@ -2,16 +2,20 @@ class WixWebShareButton extends HTMLElement {
     static observedAttributes = ["label", "share-data"];
     constructor() {super();}
     attributeChangedCallback(name, oldValue, newValue) { render() }
-    connectedCallback() { this.render(); }
+    connectedCallback() { 
+        this.render(); 
+        this.addEventListener('click', async () => {
+            try { await navigator.share(
+                {title: this.getAttribute("share-title"), text: this.getAttribute("share-text"), url: this.getAttribute("share-url")}
+            );}
+            catch (err) {console.error(`Web Share API Error: ${err}`);}
+        })
+    }
     attributeChangedCallback() { this.render(); }
     render() {
         const c = `<div data-testid="linkElement" class="VU4Mnk wixui-button wixui-tedx-button tedx-button wixui-forty-vw forty-vw wixui-no-arrow no-arrow wixui-webshare-button webshare-button PlZyDq" aria-disabled="false"><span class="w4Vxx6 wixui-button__label">${this.getAttribute("label")}</span></div>`
         this.innerHTML = c;
-        this.addEventListener('click', async () => {
-            shareDataJson = JSON.parse(this.getAttribute("share-data"));
-            try { await navigator.share(shareDataJson);}
-            catch (err) {console.error(`Web Share API Error: ${err}`);}
-        })
     }
 }
 customElements.define("tedx-wix-share-button", WixWebShareButton);
+this.getAttribute("share-data")
