01787d053e9f14b7e10fa6e8ee1075a37f50f9a2
NouveauReece <nouveaureece@protonmail.com>
2025-01-21 23:56:37 -0500

    Added iframe event listener

--- components.js
+++ components.js
@@ -37,17 +37,28 @@ class WixOpenDateEmbed extends HTMLElement {
         const frame = document.createElement("iframe");
         Object.assign(frame, {
             id: `od-confirm-${this.getAttribute("event-id")}-iframe`,
-            src: `https://app.opendate.io/confirms/${this.getAttribute("event-id")}/web_orders/new`,
             title: 'Opendate',
             scrolling: 'no',
             allowpaymentrequest: 'true',
             style: 'border:none; width: 1px; min-width: 100%;transition: all .5s ease-in-out;'
-          })
+          });
+        frame.addEventListener('load', () => {
+            this.dispatchEvent(
+                new CustomEvent("iframe-did-load", {
+                    bubbles: true,
+                    cancelable: false,
+                    composed: true
+                }),
+            );
+        })
+        frame.setAttribute('src',`https://app.opendate.io/confirms/${this.getAttribute("event-id")}/web_orders/new`);
         const resizerInit = document.createElement('script');
         resizerInit.innerText = `ODEmbed("od-confirm-${this.getAttribute("event-id")}-iframe");`;
         this.insertAdjacentElement('afterend',embedScript);
         embedScript.insertAdjacentElement('afterend',frame);
         frame.insertAdjacentElement('afterend',resizerInit);
+
+        
     }
 }
 customElements.define("tedx-wix-opendate-embed", WixOpenDateEmbed);
